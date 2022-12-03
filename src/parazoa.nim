d963d46692b2e1ed38c07984fb2c902ceca0c0eb
oakes <zach@sekao.net>
2022-12-03 12:30:47 -0500

    Add very basic map

--- src/parazoa.nim
+++ src/parazoa.nim
@@ -1,2 +1,66 @@
-func add*(x, y: int): int =
-  return x + y
+import hashes
+
+const
+  bitsPerPart = 5
+  branchWidth = 1 shl bitsPerPart
+  mask = branchWidth - 1
+  levels = int((sizeof(Hash) * 8) / bitsPerPart) - 1
+
+type
+  NodeKind* = enum
+    Leaf,
+    Branch,
+  Node*[K, V] = ref object
+    case kind: NodeKind
+    of Leaf:
+      value: V
+    of Branch:
+      nodes: array[branchWidth, Node[K, V]]
+  Map*[K, V] = ref object
+    root: Node[K, V]
+    size*: int
+
+func initMap*[K, V](): Map[K, V] =
+  new result
+  result.root = Node[K, V](kind: Branch)
+
+func copy*[K, V](node: Node[K, V]): Node[K, V] =
+  new result
+  result[] = node[]
+
+func add*[K, V](m: Map[K, V], key: K, value: V): Map[K, V] =
+  new result
+  result[] = m[]
+  result.root = copy(m.root)
+  let h = hash(key)
+  var node = result.root
+  for level in countDown(levels, 1):
+    let index = (h shr level) and mask
+    var nextNode = node.nodes[index]
+    if nextNode == nil:
+      if level > 1:
+        nextNode = Node[K, V](kind: Branch)
+      else:
+        nextNode = Node[K, V](kind: Leaf)
+        result.size += 1
+      node.nodes[index] = nextNode
+    else:
+      nextNode = copy(nextNode)
+    if level > 1:
+      node = nextNode
+    else:
+      nextNode.value = value
+
+func get*[K, V](m: Map[K, V], key: K, notFound: V): V =
+  let h = hash(key)
+  var node = m.root
+  for level in countDown(levels, 1):
+    let index = (h shr level) and mask
+    var nextNode = node.nodes[index]
+    if nextNode == nil:
+      return notFound
+    node = nextNode
+  if node == nil:
+    notFound
+  else:
+    node.value
--- tests/test1.nim
+++ tests/test1.nim
@@ -1,5 +1,17 @@
 import unittest
 import parazoa
 
-test "can add":
-  check add(5, 5) == 10
+test "maps":
+  let m1 = initMap[string, string]()
+  let m2 = m1.add("hello", "world")
+  check m1.get("hello", "") == ""
+  check m2.get("hello", "") == "world"
+  let m3 = m1.add("hello", "goodbye")
+  check m1.get("hello", "") == ""
+  check m2.get("hello", "") == "world"
+  check m3.get("hello", "") == "goodbye"
+  let m4 = m3.add("what's", "up")
+  check m1.size == 0
+  check m2.size == 1
+  check m3.size == 1
+  check m4.size == 2
